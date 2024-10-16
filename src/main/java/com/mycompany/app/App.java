a7cb5925d7251121982ffe774f7fbefa361f1523
Florent C. <florentc@users.noreply.github.com>
2024-10-16 15:57:08 +0200

    New attempt

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,9 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String password = "hardcoded_password";
-        System.out.println("The password is: " + password);
+        // Inappropriate use of System.exit()
+        if (args.length > 0) {
+            System.exit(1);
+        }
     }
 }
