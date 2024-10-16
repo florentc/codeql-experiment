963867cb8cabaecd63ecc7d37973e11b6021115b
Florent C. <florentc@users.noreply.github.com>
2024-10-16 15:51:36 +0200

    New attempt to trigger codeql report

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         String password = "hardcoded_password";
+        System.out.println("The password is: " + password);
     }
 }
